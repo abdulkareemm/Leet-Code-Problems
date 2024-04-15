6e68272394252daab6c53afb4754d57d0bc2bc8b
abdulkareemm <eng.kreem.96@gmail.com>
2024-04-15 10:55:01 +0300

    solve problem 2666

---
+++ 2666.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function (fn) {
+  let called = false;
+
+  return function (...args) {
+    if (called) {
+      return undefined;
+    }
+    called = true;
+    return fn(...args);
+  };
+};
