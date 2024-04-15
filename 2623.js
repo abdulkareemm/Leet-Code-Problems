9be24ea57b2bee1c6746616beb27f98a464e587d
abdulkareemm <eng.kreem.96@gmail.com>
2024-04-15 10:49:08 +0300

    solve problem 2623

---
+++ 2623.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+  const cache = {};
+
+  return function (...args) {
+    const key = JSON.stringify(args);
+    if (key in cache) {
+      return cache[key];
+    }
+    cache[key] = fn(...args);
+    return cache[key];
+  };
+}
